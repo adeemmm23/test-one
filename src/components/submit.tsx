e3131700e6547f727a03f233e38fdecc338f929d
Adem <112782893+adeemmm23@users.noreply.github.com>
2025-11-13 14:58:18 +0100

    fix: update Submit button styles and ensure consistent secondary color usage

--- src/components/submit.tsx
+++ src/components/submit.tsx
@@ -1,6 +1,6 @@
 export default function Submit() {
   return (
-    <button className="w-full bg-amber-300 text-zinc-900 rounded-md p-4 cursor-pointer">
+    <button className="w-full bg-secondary text-zinc-900 rounded-md p-4 cursor-pointer focus:bg-secondary/90">
       Done
     </button>
   );
