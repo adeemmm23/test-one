c316c9a1115e50a0d4652014092c07c8d552985a
Adem <112782893+adeemmm23@users.noreply.github.com>
2025-11-13 16:21:31 +0100

    fix: refactor Counter component to improve readability and maintainability

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -8,32 +8,36 @@ type CounterProps = {
 };
 
 export default function Counter({ pagesData, setPagesData }: CounterProps) {
+  const addPage = () => {
+    const newPageNumber = (pagesData.length + 1).toString();
+    const newPagesData = [
+      ...pagesData,
+      { number: newPageNumber, isChecked: false },
+    ];
+    setPagesData(newPagesData);
+  };
+
+  const removePage = () => {
+    if (pagesData.length === 0) return;
+    const newPagesData = pagesData.slice(0, -1);
+    setPagesData(newPagesData);
+  };
+
   return (
-    <div className="flex gap-3 rounded-md bg-primary p-2 fixed bottom-20 shadow-(--shadow-card)">
+    <div className="group flex gap-3 rounded-md bg-primary p-2 fixed bottom-20 shadow-(--shadow-card) transition-all hover:bg-primary/90 active:scale-95">
       <button
         disabled={pagesData.length === 0}
         className=" text-white cursor-pointer select-none disabled:opacity-50 disabled:cursor-not-allowed"
-        onClick={() => {
-          if (pagesData.length === 0) return;
-          const newPagesData = pagesData.slice(0, -1);
-          setPagesData(newPagesData);
-        }}
+        onClick={removePage}
       >
         <Minus />
       </button>
-      <span className="text-white font-medium select-none">
+      <span className="text-white font-medium select-none w-20 text-center">
         {pagesData.length} {pagesData.length === 1 ? "page" : "pages"}
       </span>
       <button
         className="text-white cursor-pointer select-none disabled:opacity-50 disabled:cursor-not-allowed"
-        onClick={() => {
-          const newPageNumber = (pagesData.length + 1).toString();
-          const newPagesData = [
-            ...pagesData,
-            { number: newPageNumber, isChecked: false },
-          ];
-          setPagesData(newPagesData);
-        }}
+        onClick={addPage}
       >
         <Plus />
       </button>
